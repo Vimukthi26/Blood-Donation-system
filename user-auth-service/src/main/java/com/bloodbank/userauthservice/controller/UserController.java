bd569b6e6d89fcb737e947aa022ce59c8eb7ed3b
lakshitha320 <lakshitha320@gmail.com>
2026-08-26 11:47:19 +0530

    Add user-auth-service with hosting tweaks

---
+++ user-auth-service/src/main/java/com/bloodbank/userauthservice/controller/UserController.java
@@ -0,0 +1,26 @@
+﻿package com.bloodbank.userauthservice.controller;
+
+import com.bloodbank.userauthservice.model.User;
+import com.bloodbank.userauthservice.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    @Autowired
+    private UserRepository repository;
+
+    @GetMapping
+    public List<User> getAll() {
+        return repository.findAll();
+    }
+
+    @PostMapping
+    public User create(@RequestBody User entity) {
+        return repository.save(entity);
+    }
+}
